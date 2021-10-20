fca94652b9f2e12641377674b07d6601ab4579b2
WhoAmI-T1T4N <92716161+WhoAmI-T1T4N@users.noreply.github.com>
2021-10-20 16:14:26 +0700

    Update ProxyBkp.sh

--- ProxyBkp.sh
+++ ProxyBkp.sh
@@ -8,13 +8,12 @@ echo "Mastah : Dont Be Lose, And Subscribe:)"
 echo "Subscribe Channel : T1T4N TZY"
 echo "____________________________________________"
 sleep 2
-echo
 echo "(1) Video Anime Bokep 1"
 echo "(2) Video Anime Bokep 2"
 echo "(3) Video Anime Bokep 3"
 echo "(4) Exit Lah Kalo Udah Tobat"
 echo 
-read -p "Pilih :"who
+read -p "Pilih :" who
 if [ $who="1" ]
 then
  echo "Buka & Salin Ini Link=www87.zippyshare.com/v/fsMLNN3i/file.html"

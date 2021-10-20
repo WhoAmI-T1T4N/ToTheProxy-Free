65a9cad2dad553985aaf6965ded8b1947a63071a
WhoAmI-T1T4N <92716161+WhoAmI-T1T4N@users.noreply.github.com>
2021-10-20 16:08:30 +0700

    Update ProxyBkp.sh

--- ProxyBkp.sh
+++ ProxyBkp.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 lolcat Video
 lolcat Bokep
 sleep 2
@@ -13,21 +13,21 @@ echo
 echo "(1) Video Anime Bokep 1"
 echo "(2) Video Anime Bokep 2"
 echo "(3) Video Anime Bokep 3"
-echo "(4) Exit Lah Kalo Udah Tobat :V"
+echo "(4) Exit Lah Kalo Udah Tobat"
 echo 
 read -p "Pilih : " 
-if [  $who = "1" ]
+if [  $who="1" ]
 then
- echo "Buka & Salin Ini Link =www87.zippyshare.com/v/fsMLNN3i/file.html"
- echo "Jangan Kelamaan Tobat :V"
-fi [ $who = "2" ]
+ echo "Buka & Salin Ini Link=www87.zippyshare.com/v/fsMLNN3i/file.html"
+ echo "Jangan Kelamaan Tobat"
+fi [ $who="2" ]
 then
   echo "Sedang Dalam Proses Update :V"
-fi [ $who = "3" ]
+fi [ $who="3" ]
 then
    echi "Jangan Kebanyakan Nonton Bokep, Tobat Bro"
   echo "Thanks"
-fi [ $who = "4" ]
+fi [ $who="4" ]
 then
 lolcat Tobat
 lolcat Bro 

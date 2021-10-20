561c5189e3910b6a6b10723d69373b81495621b9
WhoAmI-T1T4N <92716161+WhoAmI-T1T4N@users.noreply.github.com>
2021-10-20 15:52:13 +0700

    Add files via upload

---
+++ ProxyBkp.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+lolcat Video
+lolcat Bokep
+sleep 2
+echo "____________________________________________"
+echo "Author : Mr.T1T4N"
+echo "Team : Indonesia Cyber | Linux Cyber"
+echo "Mastah : Dont Be Lose, And Subscribe:)"
+echo "Subscribe Channel : T1T4N TZY"
+echo "____________________________________________"
+clear
+echo
+echo "(1) Video Anime Bokep 1"
+echo "(2) Video Anime Bokep 2"
+echo "(3) Video Anime Bokep 3"
+echo "(4) Exit Lah Kalo Udah Tobat :V"
+echo 
+read -p "Pilih : " 
+if [  $who = "1" ]
+then
+ echo "Buka & Salin Ini Link =www87.zippyshare.com/v/fsMLNN3i/file.html"
+ echo "Jangan Kelamaan Tobat :V"
+fi [ $who = "2" ]
+then
+  echo "Sedang Dalam Proses Update :V"
+fi [ $who = "3" ]
+then
+   echi "Jangan Kebanyakan Nonton Bokep, Tobat Bro"
+  echo "Thanks"
+fi [ $who = "4" ]
+then
+lolcat Tobat
+lolcat Bro 
+   echo "Udah Subscribe Channel"
+   echo "Siap Bang"
+exit
